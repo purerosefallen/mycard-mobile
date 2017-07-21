4587bd38cee97fd4765d79ebef50f236523d9a70
zh99998 <zh99998@gmail.com>
2017-07-21 21:17:21 +0800

    导航动画

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -54,8 +54,8 @@
 
 import { animate, state, style, transition, trigger } from '@angular/animations';
 export const routerTransition = trigger('routerTransition', [
-  state('void', style({ position: 'fixed', width: '100%' })),
-  state('*', style({ position: 'fixed', width: '100%' })),
+  state('void', style({ position: 'absolute', width: '100%' })),
+  state('*', style({ position: 'absolute', width: '100%' })),
   transition(':enter', [  // before 2.1: transition('void => *', [
     style({ transform: 'translateX(100%)' }),
     animate('.5s ease-in-out', style({ transform: 'translateX(0%)' }))
@@ -66,8 +66,8 @@ export const routerTransition = trigger('routerTransition', [
   ])
 ]);
 export const routerTransition2 = trigger('routerTransition2', [
-  state('void', style({ position: 'fixed', width: '100%' })),
-  state('*', style({ position: 'fixed', width: '100%' })),
+  state('void', style({ position: 'absolute', width: '100%' })),
+  state('*', style({ position: 'absolute', width: '100%' })),
   transition(':enter', [  // before 2.1: transition('void => *', [
     style({ transform: 'translateX(-100%)' }),
     animate('.5s ease-in-out', style({ transform: 'translateX(0%)' }))
