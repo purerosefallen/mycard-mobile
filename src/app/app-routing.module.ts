0405adea6a8389407233dd3ceaff0f2cb848ac66
zh99998 <zh99998@gmail.com>
2017-07-25 14:13:14 +0800

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { LobbyComponent } from './lobby/lobby.component';
 import { LoginService } from './login.service';
-import { MatchDialog } from './match/match.component';
 import { NewRoomComponent } from './new-room/new-room.component';
 import { RoomListComponent } from './room-list/room-list.component';
-import { WindbotComponent } from './windbot/windbot.component';
 import { WatchComponent } from './watch/watch.component';
-import { DecksComponent } from './decks/decks.component';
+import { WindbotComponent } from './windbot/windbot.component';
 
 const routes: Routes = [
   {
@@ -21,7 +19,6 @@ const routes: Routes = [
       { path: 'ygopro/lobby', component: LobbyComponent },
       { path: 'ygopro/windbot', component: WindbotComponent },
       { path: 'ygopro/watch', component: WatchComponent },
-      { path: 'ygopro/decks', component: DecksComponent },
     ]
   },
 ];
